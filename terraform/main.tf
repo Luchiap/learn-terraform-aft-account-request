505dfa6fda27134ad105453e55786088d5a288ca
luchiap <luchiap@amazon.com>
2023-10-10 12:26:09 -0500

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -106,3 +106,597 @@ module "accountcreation4" {
 
   account_customizations_name = "sandbox"
 }
+module "accountcreation5" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "luchiap+AFTaccountcreated5@amazon.com"
+    AccountName               = "AFTaccountcreated5"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "luchiap+AFTaccountcreated5@amazon.com"
+    SSOUserFirstName          = "Admin"
+    SSOUserLastName           = "User"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
+module "accountcreation6" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "luchiap+AFTaccountcreated6@amazon.com"
+    AccountName               = "AFTaccountcreated6"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "luchiap+AFTaccountcreated6@amazon.com"
+    SSOUserFirstName          = "Admin"
+    SSOUserLastName           = "User"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
+module "accountcreation7" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "luchiap+AFTaccountcreated7@amazon.com"
+    AccountName               = "AFTaccountcreated7"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "luchiap+AFTaccountcreated7@amazon.com"
+    SSOUserFirstName          = "Admin"
+    SSOUserLastName           = "User"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
+module "accountcreation8" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "luchiap+AFTaccountcreated8@amazon.com"
+    AccountName               = "AFTaccountcreated8"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "luchiap+AFTaccountcreated8@amazon.com"
+    SSOUserFirstName          = "Admin"
+    SSOUserLastName           = "User"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
+module "accountcreation9" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "luchiap+AFTaccountcreated9@amazon.com"
+    AccountName               = "AFTaccountcreated9"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "luchiap+AFTaccountcreated9@amazon.com"
+    SSOUserFirstName          = "Admin"
+    SSOUserLastName           = "User"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
+module "accountcreation10" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "luchiap+AFTaccountcreated10@amazon.com"
+    AccountName               = "AFTaccountcreated10"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "luchiap+AFTaccountcreated10@amazon.com"
+    SSOUserFirstName          = "Admin"
+    SSOUserLastName           = "User"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
+module "accountcreation11" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "luchiap+AFTaccountcreated11@amazon.com"
+    AccountName               = "AFTaccountcreated11"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "luchiap+AFTaccountcreated11@amazon.com"
+    SSOUserFirstName          = "Admin"
+    SSOUserLastName           = "User"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
+module "accountcreation12" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "luchiap+AFTaccountcreated12@amazon.com"
+    AccountName               = "AFTaccountcreated12"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "luchiap+AFTaccountcreated12@amazon.com"
+    SSOUserFirstName          = "Admin"
+    SSOUserLastName           = "User"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
+module "accountcreation13" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "luchiap+AFTaccountcreated13@amazon.com"
+    AccountName               = "AFTaccountcreated13"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "luchiap+AFTaccountcreated13@amazon.com"
+    SSOUserFirstName          = "Admin"
+    SSOUserLastName           = "User"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
+module "accountcreation14" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "luchiap+AFTaccountcreated14@amazon.com"
+    AccountName               = "AFTaccountcreated14"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "luchiap+AFTaccountcreated14@amazon.com"
+    SSOUserFirstName          = "Admin"
+    SSOUserLastName           = "User"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
+module "accountcreation15" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "luchiap+AFTaccountcreated15@amazon.com"
+    AccountName               = "AFTaccountcreated15"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "luchiap+AFTaccountcreated15@amazon.com"
+    SSOUserFirstName          = "Admin"
+    SSOUserLastName           = "User"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
+module "accountcreation16" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "luchiap+AFTaccountcreated16@amazon.com"
+    AccountName               = "AFTaccountcreated16"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "luchiap+AFTaccountcreated16@amazon.com"
+    SSOUserFirstName          = "Admin"
+    SSOUserLastName           = "User"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
+module "accountcreation17" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "luchiap+AFTaccountcreated17@amazon.com"
+    AccountName               = "AFTaccountcreated17"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "luchiap+AFTaccountcreated17@amazon.com"
+    SSOUserFirstName          = "Admin"
+    SSOUserLastName           = "User"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
+module "accountcreation18" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "luchiap+AFTaccountcreated18@amazon.com"
+    AccountName               = "AFTaccountcreated18"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "luchiap+AFTaccountcreated18@amazon.com"
+    SSOUserFirstName          = "Admin"
+    SSOUserLastName           = "User"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
+module "accountcreation19" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "luchiap+AFTaccountcreated19@amazon.com"
+    AccountName               = "AFTaccountcreated19"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "luchiap+AFTaccountcreated19@amazon.com"
+    SSOUserFirstName          = "Admin"
+    SSOUserLastName           = "User"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
+module "accountcreation20" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "luchiap+AFTaccountcreated20@amazon.com"
+    AccountName               = "AFTaccountcreated20"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "luchiap+AFTaccountcreated20@amazon.com"
+    SSOUserFirstName          = "Admin"
+    SSOUserLastName           = "User"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
+module "accountcreation21" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "luchiap+AFTaccountcreated21@amazon.com"
+    AccountName               = "AFTaccountcreated21"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "luchiap+AFTaccountcreated21@amazon.com"
+    SSOUserFirstName          = "Admin"
+    SSOUserLastName           = "User"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
+module "accountcreation22" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "luchiap+AFTaccountcreated22@amazon.com"
+    AccountName               = "AFTaccountcreated22"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "luchiap+AFTaccountcreated22@amazon.com"
+    SSOUserFirstName          = "Admin"
+    SSOUserLastName           = "User"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
+module "accountcreation23" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "luchiap+AFTaccountcreated23@amazon.com"
+    AccountName               = "AFTaccountcreated23"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "luchiap+AFTaccountcreated23@amazon.com"
+    SSOUserFirstName          = "Admin"
+    SSOUserLastName           = "User"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
+module "accountcreation24" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "luchiap+AFTaccountcreated24@amazon.com"
+    AccountName               = "AFTaccountcreated24"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "luchiap+AFTaccountcreated24@amazon.com"
+    SSOUserFirstName          = "Admin"
+    SSOUserLastName           = "User"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
+module "accountcreation25" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "luchiap+AFTaccountcreated25@amazon.com"
+    AccountName               = "AFTaccountcreated25"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "luchiap+AFTaccountcreated25@amazon.com"
+    SSOUserFirstName          = "Admin"
+    SSOUserLastName           = "User"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
+module "accountcreation26" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "luchiap+AFTaccountcreated26@amazon.com"
+    AccountName               = "AFTaccountcreated26"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "luchiap+AFTaccountcreated26@amazon.com"
+    SSOUserFirstName          = "Admin"
+    SSOUserLastName           = "User"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
