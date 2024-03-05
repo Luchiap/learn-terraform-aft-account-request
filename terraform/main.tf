5e6f45caa1215e5a2f9f28452fb3085457f1f1c6
luchiap <luchiap@amazon.com>
2024-03-05 14:45:09 -0600

    Create sandbox-aft Control Tower account

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ module "accountcreation1" {
 
   control_tower_parameters = {
     AccountEmail              = "luchiap+AFTaccountcreated1@amazon.com"
-    AccountName               = "AFTaccountcreated1"
+    AccountName               = "AFTaccountcreatednewname"
     ManagedOrganizationalUnit = "Sandbox"
     SSOUserEmail              = "luchiap+AFTaccountcreated1@amazon.com"
     SSOUserFirstName          = "Admin"
@@ -60,33 +60,6 @@ module "accountcreation3" {
     AccountName               = "AFTaccountcreated3"
     ManagedOrganizationalUnit = "Sandbox"
     SSOUserEmail              = "luchiap+AFTaccountcreated3@amazon.com"
-    SSOUserFirstName          = "Admin"
-    SSOUserLastName           = "User"
-  }
-
-  account_tags = {
-    "Learn Tutorial" = "AFT"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-  }
-
-  custom_fields = {
-    group = "non-prod"
-  }
-
-  account_customizations_name = "sandbox"
-}
-module "accountcreation4" {
-  source = "./modules/aft-account-request"
-
-  control_tower_parameters = {
-    AccountEmail              = "luchiap+AFTaccountcreated4@amazon.com"
-    AccountName               = "AFTaccountcreated4"
-    ManagedOrganizationalUnit = "Sandbox"
-    SSOUserEmail              = "luchiap+AFTaccountcreated4@amazon.com"
     SSOUserFirstName          = "Admin"
     SSOUserLastName           = "User"
   }
